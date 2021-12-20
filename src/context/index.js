6c7130065edb53c95644458d1d81a9ec3ab8d262
Ajingjing006 <1028126924@qq.com>
2021-12-20 13:23:06 +0800

    测试context

--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,7 @@
 import { createContext } from "react";
 export const colorContext = createContext("默认颜色");
 export const ageContext = createContext("默认年龄");
+
+//测试动态context功能
+export const publicContext = createContext({}); //默认值
+export const publicContext2 = createContext({}); //默认值
